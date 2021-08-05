adcffd4ba7271ebc35e48b4106905163c9d3d60a
Kyle Lauffer <kcl5072@gmail.com>
2021-08-05 17:59:52 -0400

    added maybe predicates

--- FunkyBasics/FunkyBasics.Tests/MaybeResultShould.cs
+++ FunkyBasics/FunkyBasics.Tests/MaybeResultShould.cs
@@ -18,5 +18,21 @@ namespace FunkyBasics.Tests
             var result = new MaybeResult<bool>.Just(true);
             Assert.True(result.Match(false, result => result));
         }
+
+        [Fact]
+        public void DetermineMaybeIsNothing()
+        {
+            var maybeResult = new MaybeResult<bool>.Nothing();
+            var booleanResult = maybeResult.IsNothing();
+            Assert.True(booleanResult.Match(() => true, () => false));
+        }
+
+        [Fact]
+        public void DetermineMaybeIsJust()
+        {
+            var maybeResult = new MaybeResult<bool>.Just(true);
+            var booleanResult = maybeResult.IsJust();
+            Assert.True(booleanResult.Match(() => true, () => false));
+        }
     }
 }
---
+++ FunkyBasics/FunkyBasics/Maybe/MaybePredicates.cs
@@ -0,0 +1,13 @@
+﻿using FunkyBasics.Boolean;
+
+namespace FunkyBasics.Maybe
+{
+    public static class MaybePredicates
+    {
+        public static BooleanResult IsNothing<T>(this MaybeResult<T> maybe) =>
+            maybe.Match<BooleanResult>(new BooleanResult.Success(), _ => new BooleanResult.Error());
+
+        public static BooleanResult IsJust<T>(this MaybeResult<T> maybe) =>
+            new BooleanResult.Not(IsNothing(maybe));
+    }
+}
