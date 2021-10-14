caddbcad64923ec91df0940efb1370d953efcc78
Kyle Lauffer <kcl5072@gmail.com>
2021-10-14 18:41:50 -0400

    First Attempt at function composition

---
+++ FunkyBasics.Tests/EitherResultCompositionShould.cs
@@ -0,0 +1,74 @@
+﻿using FunkyBasics.Either;
+using Xunit;
+
+namespace FunkyBasics.Tests
+{
+
+    public class EitherResultCompositionShould
+    {
+        [Fact]
+        public void RunFirstSuccessfully()
+        {
+            var result = IsBelow10(5).Then(MakeGreatSuccess);
+            Assert.Equal(5, result.Match(l => l, r => 0));
+        }
+
+        [Fact]
+        public void FailFirstValidation()
+        {
+            var result = IsBelow10(11).Then(IsAbove0).Then(MakeGreatSuccess);
+            Assert.Equal(ErrorMessages.NotBelow10, result.Match(l => l.ToString(), r => r));
+        }
+
+        [Fact]
+        public void FailSecondValidation()
+        {
+            var result = IsBelow10(0).Then(IsAbove0).Then(MakeGreatSuccess);
+            Assert.Equal(ErrorMessages.NotAbove0, result.Match(l => l.ToString(), r => r));
+        }
+
+        [Fact]
+        public void ShortCircuitOnFirstOfTwoValidations()
+        {
+            var result = IsBelow10(11).Then(IsEven).Then(MakeGreatSuccess);
+            Assert.Equal(ErrorMessages.NotBelow10, result.Match(l => l.ToString(), r => r));
+        }
+
+        private static EitherResult<int, string> IsBelow10(int number)
+        {
+            if (number < 10)
+                return new EitherResult<int, string>.Left(number);
+
+            return new EitherResult<int, string>.Right(ErrorMessages.NotBelow10);
+        }
+
+        private static EitherResult<int, string> IsAbove0(int number)
+        {
+            if (number > 0)
+                return new EitherResult<int, string>.Left(number);
+
+            return new EitherResult<int, string>.Right(ErrorMessages.NotAbove0);
+        }
+
+        private static EitherResult<int, string> IsEven(int number)
+        {
+            if (number%2 == 0)
+                return new EitherResult<int, string>.Left(number);
+
+            return new EitherResult<int, string>.Right(ErrorMessages.NotEven);
+        }
+
+        private static EitherResult<int, string> MakeGreatSuccess(int number)
+        {
+            return new EitherResult<int, string>.Left(number);
+        }
+
+        internal class ErrorMessages
+        {
+            public const string None = "";
+            public const string NotBelow10 = "Not Below 10!";
+            public const string NotAbove0 = "Not Above 0!";
+            public const string NotEven = "Not Even!";
+        }
+    }
+}
---
+++ FunkyBasics/Either/Railway.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FunkyBasics.Either
+{
+    public static class Composition
+    {
+        private static Func<EitherResult<T, TError>, EitherResult<TSuccess, TError>> Compose<T, TSuccess, TError>(Func<T, EitherResult<TSuccess, TError>> f)
+        {
+            return r => r.Match(t=> f(t), e => new EitherResult<TSuccess, TError>.Right(e));
+        }
+
+
+        public static EitherResult<S, TError> Then<S, TValue, TError>(this EitherResult<TValue, TError> r, Func<TValue, EitherResult<S, TError>> f) =>
+            Compose(f)(r);
+    }
+}
